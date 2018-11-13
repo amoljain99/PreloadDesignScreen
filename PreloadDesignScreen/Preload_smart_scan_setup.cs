38d65f9211bcb3a5ce706d7750922f99dc42af32
amoljain <amoljain108@gmail.com>
2018-11-13 07:26:50 +0530

    Add files via upload

---
+++ PreloadDesignScreen/Preload_smart_scan_setup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PreloadDesignScreen
+{
+    [Activity(Label = "Preload_smart_scan_setup")]
+    public class Preload_smart_scan_setup : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Preload_smart_scan_setup);
+            // Create your application here
+        }
+    }
+}
